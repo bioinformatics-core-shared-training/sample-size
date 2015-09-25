fc890ecfd8517bc24e20046ffa315e7118f36be3
markdunning <mark.dunning@gmail.com>
2015-09-25 16:30:57 +0100

    initial commit

---
+++ server.R
@@ -0,0 +1,58 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(pwr)
+library(googleVis)
+library(tidyr)
+
+shinyServer(function(input, output) {
+    datasetInput <- reactive({
+    h <- seq(.2,1.8,.01)
+    nh<-length(h)
+    h
+    
+    p <- seq(.5,.9,.1)
+    np <- length(p)
+    p
+    
+    # obtain sample sizes
+    samsize <- array(numeric(nh+np), dim=c(nh,np))
+    for (i in 1:np){
+      for (j in 1:nh){
+        result <- pwr.2p.test(n = NULL, h = h[j],
+                              sig.level = as.numeric(input$sig.level), power = p[i],
+                              alternative = input$alternative)
+        samsize[j,i] <- round(result$n)
+      }
+    }
+    data.power <- data.frame(h,samsize)
+    colnames(data.power) <- c("Effect",p)
+    data.power
+    }
+  )
+  
+  output$sSizeCalc <- renderPrint({
+    pwr.2p.test(n=NULL,h=input$effect,sig.level=as.numeric(input$sig.level),power=input$power,alternative=input$alternative)
+  
+    }
+  )
+  
+  
+  output$view <- renderGvis({
+    data <- datasetInput()
+
+    gvisScatterChart(data, options=list(title=paste("Sample sizes with significance level of", input$sig.level),width=800, height=800)
+    )
+    
+  })
+  
+  
+
+})
+  
+
---
+++ ui.R
@@ -0,0 +1,42 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Simple Sample Size Calculator"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("power",
+                  "Power",
+                  min = 0.5,
+                  max = 0.9,
+                  value = 0.8,step = 0.1),
+      sliderInput("effect",
+                  "Effect Size",
+                  min = 0.2,
+                  max = 1.8,
+                  value = 0.8,step = 0.1),
+      textInput("sig.level","Level of Significance",value = 0.05),
+      radioButtons("alternative", "Alternative", c("Two-sided"="two.sided", "Greater" = "greater", "Lower"="less"),"two.sided")
+
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Calculation",verbatimTextOutput("sSizeCalc")),
+        tabPanel("View", htmlOutput("view"))
+      )
+    )
+  )
+)
+)
